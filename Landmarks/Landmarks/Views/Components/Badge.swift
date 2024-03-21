26ef67c0957b3573bd93f891cfeacbbf082b8c5c
Gabriela Medeiros <gabriela.medeiros2010@gmail.com>
2024-03-20 22:18:07 -0300

    Drawing paths and shapes

---
+++ Landmarks/Landmarks/Views/Components/Badge.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Gabriela Medeiros da Silva on 20/03/24.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Badge()
+}
